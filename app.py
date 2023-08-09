73f98f84b00724118f179455e21797ca24c7fe62
강현욱 <bug6868@naver.com>
2023-08-09 10:46:58 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.vekhvil.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    count = len(bucket_list) + 1
+    doc = {
+        'num': count,  #버킷 등록 시, db에서 특정 버킷을 찾기 위해 'num' 이라는 고유 값 부여
+        'bucket' :bucket_receive,
+        'done' : 0   #'done' key값을 추가 해 각 버킷의 완료 상태 구분(0 = 미완료, 1 = 완료)
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': '저장 완료!'})
+    
+@app.route("/bucket/update", methods=["POST"])
+def bucket_update():
+    count_receive = int(request.form['count_give'])
+    db.bucket.update_one({'num': count_receive}, {'$set': {'done': 1}})
+    return jsonify({'msg': '상태 변경 완료!'})
+
+
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    all_buckets = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'result': all_buckets})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
